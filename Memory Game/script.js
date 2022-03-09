d685093ffad12518c724f0c3276008d182460d4b
Piermario Persico <piermariopersico@gmail.com>
2022-03-09 10:49:51 +0100

    update js

--- Memory Game/script.js
+++ Memory Game/script.js
@@ -1,7 +1,7 @@
 // queruSelector individua tramite l'id css
 const grid = document.querySelector('#grid');
 
-const cards = ['alien','bug','duck','rocket','spaceship','tiktak'];
+const cards = ['alien','bug','duck','rocket','spaceship','tiktac'];
 
 // utilizziamo lo SPREAD per unire e moltiplicare i vari array
 const deck = [...cards, ...cards];
@@ -13,17 +13,22 @@ deck.sort(function(){
 });
 // Math.random restituisce un numero casuale tra 0 e 1
 
-
-console.table(deck)
-
 for(let i = 0; i < deck.length; i++){
 
   // crea un elemento nell'HTML
   const card = document.createElement('div');
+  const cardName = deck[i];
 
 // aggiunge all'elemento la classe css card
   card.classList.add('card')
-  grid.appendChild(card)
+  card.setAttribute('data-name', cardName)
+  card.addEventListener('click', flipCard);
 
+  grid.appendChild(card)
+}
+
+function flipCard(event){
+  const card = event.target;
+  card.classList.add(card.getAttribute('data-name'),'flipped');
 
 }
