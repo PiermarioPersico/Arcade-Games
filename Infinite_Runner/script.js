72c33c8637f348826e118cef6380a1c634c91572
Piermario Persico <piermariopersico@gmail.com>
2022-03-13 16:53:32 +0100

    Update script.js

--- Infinite_Runner/script.js
+++ Infinite_Runner/script.js
@@ -11,6 +11,32 @@ const duckIdx = 1;
 const duck = road[duckIdx];
 duck.classList.add('duck');
 
+function addPlant(){
+
+  let currentPlantIdx = road.length - 1;
+  road[currentPlantIdx].classList.add('plant');
+
+
+
+  const plantInterval = setInterval(function(){
+    road[currentPlantIdx].classList.remove('plant');
+    currentPlantIdx--;
+
+    if(currentPlantIdx < 0){
+      clearInterval(plantInterval);
+      addPlant();
+      return;
+    }
+    road[currentPlantIdx].classList.add('plant');
+  }, 700)
+}
+
+  addPlant();
+
+
+
+
+
 
 function jump(event){
   if( (event.code === 'Space' || event.type === 'click') && !event.repeat ){
