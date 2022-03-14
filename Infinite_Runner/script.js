1cca5616009dc9305af3ffd4cab81f64043d94c4
Piermario Persico <piermariopersico@gmail.com>
2022-03-14 11:14:23 +0100

    ss

--- Infinite_Runner/script.js
+++ Infinite_Runner/script.js
@@ -1,24 +1,39 @@
 const road = document.querySelectorAll('#grid > div');
-console.log(road);
+const scoreEl = document.querySelector('#score');
 
-// for( let i = 0; i < road.length; i++){
-//
-//   road[i].innerText = i;
-// }
 
 // Conservo in variabile i riferimenti alla papera
 const duckIdx = 1;
 const duck = road[duckIdx];
 duck.classList.add('duck');
 
+let speed = 400;
+let score = 0;
+
+
+// Logica aggiunta pianta
 function addPlant(){
 
   let currentPlantIdx = road.length - 1;
   road[currentPlantIdx].classList.add('plant');
 
+  // if(currentPlantIdx === 1){
+  //   road[currentPlantIdx].classList.remove('plant');
+  //
+  // }
+
 
 
   const plantInterval = setInterval(function(){
+    score++;
+    scoreEl.innerText = score;
+
+    if(score % 20 === 0){
+      speed = speed - 20;
+    }
+
+
+
     road[currentPlantIdx].classList.remove('plant');
     currentPlantIdx--;
 
@@ -28,17 +43,35 @@ function addPlant(){
       return;
     }
 
+
+    // Se l'indice di posizione della pianta
+    // coincide con quello della papera
+    // e la classe `duck-jump` non è presente
+    // vuol dire che la papera ha saltato nel momento sbaglato
+    // e si è schiantata
     if(currentPlantIdx === duckIdx && !road[currentPlantIdx].classList.contains('duck-jump')){
-       showAlert('Crash!')
-       clearInterval(plantInterval);
+
        road[currentPlantIdx].classList.remove('duck');
        road[currentPlantIdx].classList.add('plant');
+       clearInterval(plantInterval);
+
+       if(score < 20){
+         showAlertOne('Ci vuole più allenamento!');
+       } else if(score < 50){
+         showAlertTwo('I risultati del lavoro si iniziano a vedere!');
+       } else if(score < 100){
+         showAlertThree('Non si scherza con Batman!');
+       }else {
+         showAlertFour('Io sono Vendetta!');
+       }
+
+
 
        return;
     }
 
     road[currentPlantIdx].classList.add('plant');
-  }, 700)
+  }, speed)
 }
 
   addPlant();
@@ -50,7 +83,7 @@ function addPlant(){
 
 function jump(event){
   if( (event.code === 'Space' || event.type === 'click') && !event.repeat ){
-    console.log(event)
+    duck.classList.remove('plant');
     duck.classList.add('duck-jump');
     setTimeout(function(){
       duck.classList.remove('duck-jump');
@@ -68,12 +101,45 @@ document.addEventListener('keydown', jump);
 
 
 // ALLERT
-function showAlert(message){
+function showAlertOne(message){
 
   const gameArea = document.querySelector('.game-area')
   const alertMessage = `
   <div class="game-alert">
-    <div class="game-alert-message">${message}<img class="winning-gif" src="../common/7561.gif" alt=""><div class="game-alert-regame"><a href="index.html">RIGIOCA</a><a href="../index.html">HOME</a></div></div>
+    <div class="game-alert-message"><p>Punteggio:${score}</p>${message}<img class="winning-gif" src="../common/1GMP.gif" alt=""><div class="game-alert-regame"><a href="index.html">RIGIOCA</a><a href="../index.html">HOME</a></div></div>
+  </div>`
+
+  gameArea.innerHTML = gameArea.innerHTML + alertMessage;
+}
+
+function showAlertTwo(message){
+
+  const gameArea = document.querySelector('.game-area')
+  const alertMessage = `
+  <div class="game-alert">
+    <div class="game-alert-message"><p>Punteggio:${score}</p>${message}<img class="winning-gif" src="../common/batmanYes.gif" alt=""><div class="game-alert-regame"><a href="index.html">RIGIOCA</a><a href="../index.html">HOME</a></div></div>
+  </div>`
+
+  gameArea.innerHTML = gameArea.innerHTML + alertMessage;
+}
+
+function showAlertThree(message){
+
+  const gameArea = document.querySelector('.game-area')
+  const alertMessage = `
+  <div class="game-alert">
+    <div class="game-alert-message"><p>Punteggio:${score}</p>${message}<img class="winning-gif" src="../common/batmanThree.gif" alt=""><div class="game-alert-regame"><a href="index.html">RIGIOCA</a><a href="../index.html">HOME</a></div></div>
+  </div>`
+
+  gameArea.innerHTML = gameArea.innerHTML + alertMessage;
+}
+
+function showAlertFour(message){
+
+  const gameArea = document.querySelector('.game-area')
+  const alertMessage = `
+  <div class="game-alert">
+    <div class="game-alert-message"><p>Punteggio:${score}</p>${message}<img class="winning-gif" src="../common/batmanFour.gif" alt=""><div class="game-alert-regame"><a href="index.html">RIGIOCA</a><a href="../index.html">HOME</a></div></div>
   </div>`
 
   gameArea.innerHTML = gameArea.innerHTML + alertMessage;
