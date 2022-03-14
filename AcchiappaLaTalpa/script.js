2ed05e1ba45b5c8b1c647bcf76d9c59425f193df
Piermario Persico <piermariopersico@gmail.com>
2022-03-14 09:47:33 +0100

    ss

--- AcchiappaLaTalpa/script.js
+++ AcchiappaLaTalpa/script.js
@@ -1,84 +1,85 @@
 
-// inseriamo punteggio iniziale
-const scoreDisplay = document.querySelector('#score-display');
-let score = 0;
-scoreDisplay.innerText = score;
+  // inseriamo punteggio iniziale
+  const scoreDisplay = document.querySelector('#score-display');
+  let score = 0;
+  scoreDisplay.innerText = score;
 
 
-// inseriamo il tempo iniziale
-const timerDisplay = document.querySelector('#timer-display');
-let timeLeft = 30;
-timerDisplay.innerText = timeLeft;
-
-
-//inseriamo il bug via JS
-const cells = document.querySelectorAll('.cell');
-
-//diamo un valore di velocità iniziale
-
-let bugSpeed = 800; //millisecondi
-
-
-//Math.random restituisce un numero da 0 a 1 mentre Math.floor lo semplifica per difetto.in questo caso otteniamo un numero tra 0 e 8
-function randomBug (){
-
-  removeBug();
-
-  const randomNumber = Math.floor(Math.random() * cells.length);
-  const cell = cells[randomNumber];
-  cell.classList.add('bug');
-}
-
-const bugMovement = setInterval(randomBug, bugSpeed);
-
-
-//funzione per pulire le celle prima di inserire un altro bug
-function removeBug(){
-  for(i = 0; i < cells.length; i++){
-    const cellToClean = cells[i];
-    cellToClean.classList.remove('bug');
-  }
-}
-
-//facciamo colpire il bug dall'utente
-for(let i = 0; i < cells.length; i++){
-  const cell = cells[i];
-
-  cell.addEventListener('click', function(){
-    if(cell.classList.contains('bug')){
-      score++;
-      scoreDisplay.innerText = score;
-      console.log(score);
-      cell.classList.remove('bug');
-      cell.classList.add('splat');
-
-      setTimeout(function(){
-//puliamo la cella dallo splat
-       cell.classList.remove('splat');
-
-      },200)
-    }
-  })
-}
-
-
-
-
-//conto alla rovescia
-const timer = setInterval(countDown, 1000)
-
-function countDown(){
-  timeLeft--;
+  // inseriamo il tempo iniziale
+  const timerDisplay = document.querySelector('#timer-display');
+  let timeLeft = 30;
   timerDisplay.innerText = timeLeft;
 
 
-  if(timeLeft === 0){
-    clearInterval(timer);
-    clearInterval(bugMovement);
+  //inseriamo il bug via JS
+  const cells = document.querySelectorAll('.cell');
+
+  //diamo un valore di velocità iniziale
+
+  let bugSpeed = 800; //millisecondi
+
+
+  //Math.random restituisce un numero da 0 a 1 mentre Math.floor lo semplifica per difetto.in questo caso otteniamo un numero tra 0 e 8
+  function randomBug (){
+
     removeBug();
-    showAlert(`FINE PARTITA! Hai fatto ${score} punti!`)
+
+    const randomNumber = Math.floor(Math.random() * cells.length);
+    const cell = cells[randomNumber];
+    cell.classList.add('bug');
   }
-}
+
+  const bugMovement = setInterval(randomBug, bugSpeed);
+
+
+  //funzione per pulire le celle prima di inserire un altro bug
+  function removeBug(){
+    for(i = 0; i < cells.length; i++){
+      const cellToClean = cells[i];
+      cellToClean.classList.remove('bug');
+    }
+  }
+
+  //facciamo colpire il bug dall'utente
+  for(let i = 0; i < cells.length; i++){
+    const cell = cells[i];
+
+    cell.addEventListener('click', function(){
+      if(cell.classList.contains('bug')){
+        score++;
+        scoreDisplay.innerText = score;
+        console.log(score);
+        cell.classList.remove('bug');
+        cell.classList.add('splat');
+
+        setTimeout(function(){
+          //puliamo la cella dallo splat
+          cell.classList.remove('splat');
+
+        },200)
+      }
+    })
+  }
+
+
+
+
+  //conto alla rovescia
+  const timer = setInterval(countDown, 1000)
+
+  function countDown(){
+    timeLeft--;
+    timerDisplay.innerText = timeLeft;
+
+
+    if(timeLeft === 0){
+      clearInterval(timer);
+      clearInterval(bugMovement);
+      removeBug();
+      showAlert(`FINE PARTITA! Hai fatto ${score} punti!`)
+    }
+  }
+
 
 
 
